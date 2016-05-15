36593d7e0ce8036d265968d40ec36c34b55a5074
nvvinogradov@mail.ru <nvvinogradov@mail.ru>
2016-05-15 18:21:02 +0300

    My first commit in git

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Nik
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World !!!"); /* prints !!!Hello World!!! */
+	puts("!!!Commit 1 !!!");
+	return EXIT_SUCCESS;
+}
